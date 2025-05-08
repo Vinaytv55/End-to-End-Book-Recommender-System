24004d1684b3f62d1eab2f51c1b810e884ad3bf1
Vinay T V <vinaytv1256@gmail.com>
2025-05-08 23:42:50 +0530

    utils

--- test.py
+++
@@ -1,9 +0,0 @@
-from books_recommender.exception.exception_handler import AppException
-##books_recommender\exception\exception_handler
-import sys
-
-
-try:
-    a = 2/0
-except Exception as e:
-    raise AppException(e, sys) from e
