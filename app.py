ce6a48a17bf130c4dc03994b2f051420c421903a
Vinay T V <vinaytv1256@gmail.com>
2025-05-08 19:19:56 +0530

    log

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 
 from books_recommender.exception.exception_handler import AppException
 import sys
+from books_recommender.logger.log import logging
 
 try:
     a = 2/0
 except Exception as e:
+
+    logging.info()
     raise AppException(e, sys) from e
